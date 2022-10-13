90741460550be5b55bd9a2be4c4612360b0c8175
Andrew Gassiot <gassiot.andrew@gmail.com>
2022-10-13 08:20:43 -0500

    renamed files, started converting handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const familyRoutes = require('./familyRoutes');
 
-router.use('/', homeRoutes);
+router.use('/', familyRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
