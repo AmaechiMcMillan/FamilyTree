402781dc4859082d1e91cb75b0cd9e53c4a2a0b1
Andrew Gassiot <gassiot.andrew@gmail.com>
2022-10-13 11:38:30 -0500

    seeds n models

---
+++ models/Fam.js
@@ -0,0 +1,32 @@
+
+Fam.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    father: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    mother: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'fam',
+  }
+);
+
+module.exports = Fam;
---
+++ seeds/seed.js
@@ -0,0 +1,24 @@
+const sequelize = require('../config/connection');
+const { User, Fam } = require('../models');
+
+const userData = require('./userData.json');
+const famData = require('./famData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Fam.bulkCreate(famData,{
+    individualHooks: false,
+    returning: true,
+    });
+
+
+  process.exit(0);
+};
+
+seedDatabase();
