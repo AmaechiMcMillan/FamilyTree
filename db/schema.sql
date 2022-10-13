f8e1b76c82e53153f313640d6e3877ddcef1e40f
Amaechi McMillan <amaechimcmillan@gmail.com>
2022-10-13 14:17:59 -0500

    before merge

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,24 @@
 DROP DATABASE IF EXISTS familytree_db;
 CREATE DATABASE familytree_db;
 
-USE familytree_db;
+USE family_db;
 
-CREATE TABLE user (
-  id INT NOT NULL,
-  email VARCHAR(30),
-  username VARCHAR(30),
-  password VARCHAR(30)
-);
+-- CREATE TABLE user (
+--   id INT NOT NULL,
+--   email VARCHAR(30),
+--   username VARCHAR(30),
+--   password VARCHAR(30)
+-- );
 
-CREATE TABLE person (
-  id INT NOT NULL,
-  user_id INT NOT NULL,
-  parent VARCHAR(30),
-  spouse_of varchar(30)  
-)
+-- CREATE TABLE person (
+--   id INT NOT NULL,
+--   user_id INT NOT NULL,
+--   parent VARCHAR(30),
+--   spouse_of varchar(30)  
+-- )
 
-CREATE TABLE personHasChild (
-  person_id INT NOT NULL,
-  child_id INT NOT NULL  
-)
+-- CREATE TABLE personHasChild (
+--   person_id INT NOT NULL,
+--   child_id INT NOT NULL  
+-- )
 
